15bfd41602f297683e663c412676b43a3ee295e6
sethbarryberg <bergseth98@gmail.com>
2023-07-02 16:38:16 -0700

    NEW VERSION SECOND ATTEMPT, put all inputs into one line instead of multiple lines to try to get rid of the error I was getting from the automatic grader despite my code working as instructed

---
+++ average.py
@@ -0,0 +1,11 @@
+print("please enter five numbers" )
+
+number1, number2, number3, number4, number5 = float(input()),float(input()),float(input()),float(input()),float(input())
+print(number1, number2, number3, number4, number5)
+# = float(input())
+# = float(input())
+# = float(input())
+#number5 = float(input())
+avg = (number1 + number2 + number3 + number4 + number5) / 5
+print("The average of your five numbers: ")
+print(avg)
