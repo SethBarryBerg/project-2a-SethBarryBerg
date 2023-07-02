dbf716067a7a8ac38b1aa7c8b668c6df04615180
sethbarryberg <bergseth98@gmail.com>
2023-07-02 16:46:39 -0700

    NEW VERSION THIRD ATTEMPT, put all inputs into one line instead of multiple lines to try to get rid of the error I was getting from the automatic grader despite my code working as instructed
    added lines explaining my problems

--- average.py
+++ average.py
@@ -2,10 +2,25 @@ print("please enter five numbers" )
 
 number1, number2, number3, number4, number5 = float(input()),float(input()),float(input()),float(input()),float(input())
 print(number1, number2, number3, number4, number5)
-# = float(input())
-# = float(input())
-# = float(input())
-#number5 = float(input())
 avg = (number1 + number2 + number3 + number4 + number5) / 5
 print("The average of your five numbers: ")
 print(avg)
+
+# NOWHERE in the instructions/modules is there anything that tells us how the code can recieve an input all
+# in one line to avoid the Test Failed: list index out of range error.
+# This has cost me literally three hours of my time just to try to fix.
+# The code works as it should and outputs the format exactly as instructed.
+# Please don't dock me points I really tried
+# Show us how to do it correctly so we can learn
+
+#I also tried the code as such:
+
+#print("please enter five numbers" )
+#number1 = float(input())
+#number2 = float(input())
+#number3 = float(input())
+#number4 = float(input())
+#number5 = float(input())
+#avg = (number1 + number2 + number3 + number4 + number5) / 5
+#print("The average of your five numbers: ")
+#print(avg)
